06099ccbd945b9764fc4e40245f2a5cf5cd47235
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-02-12 12:04:20 -0600

    circular

---
+++ HT2Estructuras/__MACOSX/HT2Estructuras/HT2Estructuras/src/ht2estructuras/circular.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ht2estructuras;
+
+/**
+ *
+ * @author SergioMarchena
+ * @author Mafer Lopez
+ */
+public class circular <E> extends Lista<E>{
+    
+    protected Node<E> tail; 
+protected int count;
+
+public CircularList()
+// pre: constructs a new circular list
+{
+   tail = null;
+   count = 0;
+}
+
+public void addFirst(E value)
+// pre: value non-null
+// post: adds element to head of list
+{
+   Node<E> temp = new Node<E>(value);
+   if (tail == null) { // first value added
+       tail = temp;
+       tail.setNext(tail);
+   } else { // element exists in list
+       temp.setNext(tail.next());
+       tail.setNext(temp);
+   }
+   count++;
+}
+
+
+public void addLast(E value)
+// pre: value non-null
+// post: adds element to tail of list
+{
+   // new entry:
+   addFirst(value);
+   tail = tail.next();
+}
+
+
+// lo dificil es quitar el elemento de la cola
+
+public E removeLast()
+// pre: !isEmpty()
+// post: returns and removes value from tail of list
+{
+   Node<E> finger = tail;
+   while (finger.next() != tail) {
+       finger = finger.next();
+   }
+   // finger now points to second-to-last value
+   Node<E> temp = tail;
+   if (finger == tail)
+   {
+       tail = null;
+   } else {
+       finger.setNext(tail.next());
+       tail = finger;
+   }
+   count--;
+   return temp.value();
+    
+}
